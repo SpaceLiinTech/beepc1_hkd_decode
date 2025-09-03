81f92d9f04a372fe91aec077ff0c247dda6a937b
VUCA <97001618+vhpham94@users.noreply.github.com>
2025-09-03 18:21:42 +0900

    Create hkd_decode_simplified.py

---
+++ hkd_decode_simplified.py
@@ -0,0 +1,20 @@
+import os, sys
+def decode(fn):
+    with open(fn, "rb") as f:
+        data = f.read()
+    print(len(data))
+    i = 0
+    while i < len(data):
+        if data[i] == 0xAA:
+            if data[i+2] == 1 and i + 6 < len(data):
+                print(f"\n{data[i+3]}-{data[i+4]}{data[i+5]}{data[i+6]}:",end='')
+                i+=7
+            elif data[i+2] == 2 and i + 5 < len(data):
+                print(f"{data[i+3]}-{int.from_bytes(data[i+4:i+6],byteorder='little',signed=True)},",end='')
+                i+=6
+        i+=1
+
+files = os.listdir(sys.argv[1])
+files.sort()
+for file in files:
+    decode(f"{sys.argv[1]}/{file}")
